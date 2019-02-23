466c367b1a37a2cf11533bbd7c8abc766ba9f869
chenhan <598202794@qq.com>
2019-02-23 10:49:00 +0800

    asd

---
+++ src/main/java/com/Zjzx_app_zull9091.java
@@ -0,0 +1,26 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+@SpringBootApplication
+@EnableZuulProxy
+public class Zjzx_app_zull9091 {
+	public static void main(String[] args) {
+		SpringApplication.run(Zjzx_app_zull9091.class, args);
+	}
+}
+
+// public class ZjzxBootControllerApplication extends
+// SpringBootServletInitializer{
+// @Override
+// protected SpringApplicationBuilder configure(
+// SpringApplicationBuilder builder) {
+// // TODO Auto-generated method stub
+// return builder.sources(ZjzxBootControllerApplication.class);
+// }
+// public static void main(String[] args) {
+// SpringApplication.run(ZjzxBootControllerApplication.class, args);
+// }
+// }
+
